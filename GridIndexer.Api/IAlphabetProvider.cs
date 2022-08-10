d2a2a2b3d0d39a1767a59f59e1f02069a4ede2f8
Martin Irwin <>
2022-08-10 15:52:33 +0100

    martin irwin solution

---
+++ GridIndexer.Api/IAlphabetProvider.cs
@@ -0,0 +1,6 @@
+namespace InvantiTestApi;
+
+public interface IAlphabetProvider 
+{
+    IEnumerable<char>GetAlphabetCharacters();
+}
--- GridIndexer.Api/Indexers/GridIndexer.cs
+++ GridIndexer.Api/Indexers/GridIndexer.cs
@@ -2,6 +2,16 @@
 
 public class GridIndexer
 {
+    private readonly IDictionary<char, int> _charToIntDict = new Dictionary<char, int>();
+    private readonly IDictionary<int, char> _intToCharDict = new Dictionary<int, char>();
+    private IAlphabetProvider _provider;
+
+    public GridIndexer(IAlphabetProvider provider)
+    {
+        _provider = provider;
+        _setupDictionaries();
+    }
+
     /// <summary>
     /// Converts a string index to a row and col numbers.
     /// </summary>
@@ -10,7 +20,33 @@ public class GridIndexer
     /// <exception cref="FormatException"></exception>
     public (int row, int col) GetCoordinateFromIndex(string index)
     {
-        return (1, 1);
+        var clean = index.ToUpper().Replace(" ", "");
+
+        try
+        {
+            (int row, string letters) = _splitLettersAndNumbers(clean);
+
+            var last = letters[letters.Length - 1];
+            var remainder = _charToIntDict[last];
+            var power = letters.Length - 1;
+            var total = 0.0;
+
+            for (int i = 0; i < letters.Length - 1; i++) // less than only is because of remainder
+            {
+                var key = _charToIntDict[letters[i]] + 1; // get letter index
+
+                total += key * Math.Pow(_charToIntDict.Count, power);
+                power--;
+            }
+
+            total += remainder;
+
+            return (row, (int)total);
+        }
+        catch (FormatException ex)
+        {
+            throw ex;
+        }
     }
 
     /// <summary>
@@ -20,8 +56,95 @@ public class GridIndexer
     /// <param name="row"></param>
     /// <param name="col"></param>
     /// <returns></returns>
+    /// <exception cref="FormatException"></exception>
     public string GetIndexFromCoordinate(int row, int col)
     {
-        return "";
+        try
+        {
+            _getLettersLength(col);
+        }
+        catch (Exception ex)
+        {
+            throw new FormatException(ex.Message);
+            //return ex.Message; not sure - exception could be a breaking change if someone was using this
+        }
+
+        var rowOut = row + 1;
+        var count = col + 1;
+        var colStr = "";
+
+        while (count > 0)
+        {
+            var mod = (count - 1) % _charToIntDict.Count;
+            count -= mod;
+
+            colStr = _intToCharDict[mod].ToString() + colStr;
+
+            count = (int)(count / _charToIntDict.Count);
+        }
+
+        return $"{colStr}{rowOut}";
+    }
+
+    private void _setupDictionaries()
+    { // sets up char/int dictionaries that are used to translate char/int values throughout the class
+
+        var chars = _provider.GetAlphabetCharacters().ToList();
+
+        for (int i = 0; i < chars.Count; i++)
+        {
+            _charToIntDict.Add(chars[i], i);
+            _intToCharDict.Add(i, chars[i]);
+        }
+    }
+
+    private (int row, string letters) _splitLettersAndNumbers(string idx) // separate input: "AA28" --> "AA" & "28"
+    {
+        var clean = idx.ToUpper().Replace(" ", "");
+        var letters = "";
+        var nums = "";
+
+        for (int i = 0; i < clean.Length; i++)
+        {
+            if (char.IsDigit(clean[i]))
+            {
+                nums += clean[i].ToString();
+                continue;
+            }
+            if (char.IsLetter(clean[i]))
+            {
+                letters += clean[i].ToString();
+                continue;
+            }
+            throw new FormatException("unknown character in provided index");
+        }
+
+        if (!int.TryParse(nums.ToString(), out int row))
+        {
+            throw new FormatException("unable to parse numeric portion of input");
+        }
+
+        if (string.IsNullOrEmpty(letters))
+        {
+            throw new FormatException("no letters in input");
+        }
+
+        row--;
+        return (row, letters);
+    }
+
+    private int _getLettersLength(int col) // number of letters in returned index
+    {
+        var total = 0.0;
+
+        for (int i = 1; i <= _intToCharDict.Count; i++)
+        {
+            total += Math.Pow(_intToCharDict.Count, i);
+
+            if (total <= col) continue;
+            return i;
+        }
+
+        throw new ArgumentOutOfRangeException(nameof(col), "column cannot be represented by provided alphabet");
     }
 }
